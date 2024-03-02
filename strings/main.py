a606ed2d382a84fcdb02396e91c1aae94281bd54
Cho Phan <callmechophan@gmail.com>
2024-03-02 16:36:50 +0700

    1.1.3 strings

---
+++ strings/main.py
@@ -0,0 +1,26 @@
+# - string: 
+#   + is a sequence of character
+#   + enclosed in single quote or double quote
+
+# single line
+a = "hello"
+print(a)
+
+# multiple line
+b = "this is too " \
+    "big to fit"
+print(b)
+
+# reassigning a string value
+c = "cho"
+c = "phan"
+print(c)
+
+# sequence
+d = "cho"
+print(d[1])
+
+# len()
+e = "hello"
+print(len(e))
+
