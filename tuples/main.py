bd80496a2925a14c84ab85a160ea21996d2ba41e
Cho Phan <callmechophan@gmail.com>
2024-03-03 01:39:55 +0700

    2.1.4 tuples

---
+++ tuples/main.py
@@ -0,0 +1,7 @@
+tuple1 = (1, "strings", 4.5, True)
+
+# count: count element in tuple with specific element
+print(tuple1.count(4.5))
+
+# index: get the first index of element with specific element
+print(tuple1.index(1))
