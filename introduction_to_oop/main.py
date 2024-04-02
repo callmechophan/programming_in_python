5ba87975bc794eed74e7ce0c8b818450a8ad6977
Cho Phan <callmechophan@gmail.com>
2024-04-02 23:50:42 +0700

    3.3.1 introduction to oop

---
+++ introduction_to_oop/main.py
@@ -0,0 +1,17 @@
+# inheritance
+# - creating a new class which is derivative of an existing one
+# 
+# polymorphism
+# - the ability of a function to change its behavior when called by different objects
+# 
+# encapsulation
+# - limits access to method and variables by encasing them in a single unit of scope
+# 
+# abstraction
+# - hides implementation details for data security
+# 
+# other OOP concepts
+# - method overloading
+# - method overriding
+# - constructors
+# - ...
