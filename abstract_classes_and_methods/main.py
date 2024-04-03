815556c26b6fc270742bf73a2d264b58e188826d
Cho Phan <callmechophan@gmail.com>
2024-04-03 22:15:57 +0700

    3.3.5 abstract classes and methods

---
+++ abstract_classes_and_methods/main.py
@@ -0,0 +1,7 @@
+# abstract class
+# - cannot create an instance
+# - does not support abstraction directly
+# - can have one or more abstract methods
+# 
+# child of abstract class
+# - can only be instantiated if abstract methods are overridden
