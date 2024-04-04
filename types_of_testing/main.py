2ed540a2a5ab538b4d9d59012bf8a206d2a5c231
Cho Phan <callmechophan@gmail.com>
2024-04-04 16:14:12 +0700

    4.3.1 types of testing

---
+++ types_of_testing/main.py
@@ -0,0 +1,21 @@
+# four level in testing
+# - unit testing
+# - integration testing
+# - system testing
+# - acceptance testing
+# 
+# unit/component testing
+# - components are low-level
+# - developers write tests while writing code
+# 
+# integration testing
+# - combines the unit tests and test the flow of data from one component to another
+# 
+# system testing
+# - which tests all the software you tested against the set requirements and expectations to ensure completeness
+# - reliability
+# - performance
+# - security
+# - load balancing
+# 
+# acceptance testing
